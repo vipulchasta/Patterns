9803cd5521780e6986f504a037a961ef0161408d
Vipul Chasta <chasta.vipul@gmail.com>
2016-12-30 21:07:16 +0530

    Create 1.cpp

---
+++ Floyds Tringle/1.cpp
@@ -0,0 +1,34 @@
+/*
+  *Floyds Tringle
+  
+  When input is 5
+  1
+  2 3
+  4 5 6
+  7 8 9 10
+  11 12 13 14 15
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    int countNum = 1;
+    for( int i = 0 ; i <= n ; i++ )
+    {
+        for( int j = 0 ; j < i ; j++ )
+        {
+            cout << countNum << " " ;
+            countNum++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
